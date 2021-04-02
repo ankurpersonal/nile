2be1d09b8896259ab43ad381719cf47919ae2f0a
ankurpersonal <ankurvijay.edu@gmail.com>
2021-04-02 15:11:10 +0530

    creating multiple records in controller

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Books Api", type: :request do
 	describe "GET /books" do
 		before do
-			FactoryBot.create(:book, title:'1', author: 'sa')
+			authour = FactoryBot.create(:author, first_name:'test1', last_name: 'best', age:25)
 			FactoryBot.create(:book, title:'3', author: 'asdf')			
 		end
 		
@@ -16,28 +16,43 @@ describe "Books Api", type: :request do
 	end
 
 	describe "POST /books" do
+		# it "create a new book" do
+		# 	expect {
+		# 		post '/api/v1/books', params: { book: {title: "the martian", author: 'andy wire'} }
+		# 	}.to change { Book.count }.from(0).to(1)
+		
+		# 	expect(response).to have_http_status(:created)  
+		# end
+
 		it "create a new book" do
 			expect {
-				post '/api/v1/books', params: { book: {title: "the martian", author: 'andy wire'} }
+				post '/api/v1/books', params: {
+					book: {title: "the martian"},
+					author: {first_name: 'andy ', last_name: "wire", age: 48}
+				 }
 			}.to change { Book.count }.from(0).to(1)
 		
-			expect(response).to have_http_status(:created)  
+			expect(response).to have_http_status(:created) 
+			expect(Author.count).to eq(1)   
 		end
+
+
+
 	end
 	
-	describe "DELETE " do
-	  context "/books/:id" do
-		let!(:book) { FactoryBot.create(:book, title: '1984', author: 'george') }  #we added ! because to stop the lazy loading for book var,  it helps create book first in test db before full test execute 
+	# describe "DELETE " do
+	#   context "/books/:id" do
+	# 	let!(:book) { FactoryBot.create(:book, title: '1984', author: 'george') }  #we added ! because to stop the lazy loading for book var,  it helps create book first in test db before full test execute 
 
-		it "deletes a book" do
-			expect {
-				delete "/api/v1/books/#{book.id}"
-			}.to change { Book.count }.from(1).to(0)
+	# 	it "deletes a book" do
+	# 		expect {
+	# 			delete "/api/v1/books/#{book.id}"
+	# 		}.to change { Book.count }.from(1).to(0)
 
 		  
 
-		  expect(response).to have_http_status(:no_content)  
-		end
-	  end
-	end
+	# 	  expect(response).to have_http_status(:no_content)  
+	# 	end
+	#   end
+	# end
 end
