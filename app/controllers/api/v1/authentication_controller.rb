0605cf05d292b62c155284d92819a5109b334d87
ankurpersonal <ankurvijay.edu@gmail.com>
2021-04-02 21:29:35 +0530

    added password field for user and has_Secure_password by bycrpt

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,11 +1,14 @@
 module Api
     module V1
       class AuthenticationController < ApplicationController
+        class AuthenticationError < StandardError; end 
+
         rescue_from ActionController::ParameterMissing, with: :parameter_missing
+        rescue_from AuthenticationError, with: :handle_unauthenticated
         def create   
-            p params.require(:username).inspect
-            p params.require(:password).inspect
-            user = User.find_by(username: params.require(:username))
+            # p params.require(:password).inspect
+                          
+            raise AuthenticationError unless user.authenticate(params.require(:password))
             token = AuthenticationTokenService.call(user.id)
 
             render json: { token: token} , status: :created
@@ -14,6 +17,14 @@ module Api
 
         private
 
+        def user
+            @user ||= User.find_by(username: params.require(:username))
+
+        end
+
+        def handle_unauthenticated
+            head :unauthorized
+        end
         def parameter_missing(e)
             render json: {error: e.message}, status: :unprocessable_entity
         end
--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe "Authentication", type: :request do
     describe "POST authenticate" do
-        let(:user) {FactoryBot.create(:user, username: 'BookeSeller99')}
+        let(:user) {FactoryBot.create(:user, username: 'BookeSeller99', password: 'password')}
 
         it "authenticate the client" do
             post '/api/v1/authenticate', params: { username: user.username, password: 'password'}
             expect(response).to have_http_status(:created)
             expect(response_body).to eq({
-                'token' => '123'
+                'token' => 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.DiPWrOKsx3sPeVClrm_j07XNdSYHgBa3Qctosdxax3w'
             })   
         end
  
@@ -28,6 +28,11 @@ describe "Authentication", type: :request do
             })  
         end
         
+        it "returns error whrn password is incorrect" do
+            post '/api/v1/authenticate', params: { username: user.username, password: 'incorrect' }
+            expect(response).to have_http_status(:unauthorized)
+
+        end
         
         
     end
